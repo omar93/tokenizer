3740829e6ec6e2e85612183a472fd395aed99013
omar93 <omar93@hotmail.se>
2021-10-21 13:27:14 +0200

    Update app.js
    added more classes to be used for the tokenizer like a grammar class and a token class

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-export default class Tokenizer {
+export class Tokenizer {
 
   constructor(userInput, grammar) {
     this.position = 0
-    this.regexArr = grammar.regex
+    this.grammar = grammar
+    this.grammarArray = grammar.getGrammar()
     this.userInput = userInput.trim()
     this.restOfString = ''
     this.maximalMunch = 0
@@ -11,7 +12,8 @@ export default class Tokenizer {
     this.foundMatch
     this.error = 'END'
     this.resetValues()
-    this.runRegexsAgainstToken(this.userInput,this.regexArr)
+    // this.grammar.runRegexsAgainstToken(this.userInput)
+    this.runRegexsAgainstToken(this.userInput,this.grammarArray)
   }
 
   resetValues() {
@@ -19,33 +21,50 @@ export default class Tokenizer {
     this.position = 0
     this.matches = []
   }
+
   setString(newUserInput) {
     this.resetValues()
     this.userInput = newUserInput.trim()
     this.runRegexsAgainstToken(this.userInput,this.regexArr)
   }
 
-  runRegexsAgainstToken (word, regexArr) {
+  runRegexsAgainstToken (word, grammarArray) {
     if(this.userInput === '') {
       this.match = {'value':'END','tokenType':'END','regex':'No regex'}
     } else {
     this.maximalMunch = 0
     this.foundMatch = false
-    regexArr.forEach(reg => {
+    grammarArray.forEach(reg => {
       let match = word.match(reg.regex)
-      if(this.isMatchAndNotNull(match) && this.checkMaximalMunch(match[0].length,reg)) {
-        this.setMatchStatusAndValue(match,reg)
-      }
+      this.checkCorrectMatch(match,reg)
     })
-
-    if(!this.foundMatch) {
-      throw new TypeError('Lexical error')
-    }
-    
-    this.restOfString = word.replace(this.match.value,'').trim()
-    this.matches.push(this.match)
+    this.checkMatchError(this.foundMatch)
+    this.setMatch(word)
     }
   }
+
+  checkCorrectMatch(match,reg) {
+    if(this.isMatchAndNotNull(match) && this.checkMaximalMunch(match[0].length,reg)) {
+      this.setMatchStatusAndValue(match,reg)
+    }
+  }
+
+  checkMatchError(match) {
+    if(!match) {
+      throw new TypeError('Lexical error')
+    }
+  }
+
+  setMatch(word) {
+    this.restOfString = word.replace(this.match.getTokenValue(),'').trim()
+    this.matches.push(this.match)
+  }
+
+  setMatchStatusAndValue = (match,reg) => {
+    this.foundMatch = true
+    this.match = new Tokentype(match[0], reg)
+  }
+
 
   isMatchAndNotNull = match => {
     if(match || match != null) {
@@ -60,11 +79,6 @@ export default class Tokenizer {
       return true
     }
   }
-
-  setMatchStatusAndValue = (match,reg) => {
-    this.foundMatch = true
-    this.match = {'value':match[0],'tokenType':reg.type,'regex':reg.regex}
-  }
     
   next = _ => {
     this.position++
@@ -74,7 +88,7 @@ export default class Tokenizer {
     if(this.restOfString.length > 0) {
       if(this.position <= this.matches.length || !this.matches[this.position]) {
         try {
-          this.runRegexsAgainstToken(this.restOfString,this.regexArr)
+          this.runRegexsAgainstToken(this.restOfString,this.grammarArray)
         } catch (e) {
           throw new TypeError('Lexical error')
         }
@@ -88,4 +102,48 @@ export default class Tokenizer {
   }
   getCurrentToken = _ => this.position < 0 || this.position >= this.matches.length ?  {'value':this.error,'tokenType':this.error} : this.matches[this.position]
   getAllMatchedTokens = _ => this.matches
+  getGrammar = _ => this.grammar
+  getUserInput = _ => this.userInput
+}
+
+export class Grammar {
+
+    constructor(regex) {
+        this.grammarList = regex ?? new Array
+    }
+
+    setGrammar = regexArray => {
+        this.grammarList = regexArray
+    }
+
+    addGrammar = grammar => {
+        this.grammarList.push(grammar)
+    }
+
+    getGrammar = _ => this.grammarList
+    
+}
+
+export class Tokentype {
+
+    constructor(word,regex) {
+        this.token = {
+            'value':word,
+            'tokenType': regex.type,
+            'sentenceEnding': regex.sentenceEnding,
+            'regex': regex.regex
+        }
+    }
+
+    getTokenValue() {
+        return this.token.value
+    }
+
+    getTokenType() {
+        return this.token.tokenType
+    }
+
+    getTokenEnding() {
+        return this.token.sentenceEnding
+    }
 }
